6174ca64686ebd87a080c8f0cd939bff757de19e
Guillermo Costamaga <costamagaguillermo@gmail.com>
2025-09-18 21:11:49 -0300

    Respuesta simple

--- src/principal/views.py
+++ src/principal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def saludar(request):
+    return HttpResponse("Hola Guille!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1>Hola Guillo!!!!<h1>")
