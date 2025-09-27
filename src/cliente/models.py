0d121901710269692e0d1cf3cfa42afb23eebdb5
Guillermo Costamaga <costamagaguillermo@gmail.com>
2025-09-27 19:25:27 -0300

    Uso app admin

--- src/cliente/models.py
+++ src/cliente/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self) -> str:
+        return f'{self.nombre}'
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     fec_nac = models.DateField(null=True, blank=True)
     pais_origen = models.ForeignKey(Pais, on_delete=models.PROTECT, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.nombre} {self.apellido}'
     
