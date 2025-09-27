72cc7d7bec22e75965acc08d3d719aabf2aa70b6
Guillermo Costamaga <costamagaguillermo@gmail.com>
2025-09-27 20:02:52 -0300

    Personalizacion de admin

--- src/cliente/admin.py
+++ src/cliente/admin.py
@@ -4,4 +4,13 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Pais)
-admin.site.register(models.Cliente)
+#admin.site.register(models.Cliente)
+
+@admin.register(models.Cliente)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "fec_nac", "pais_origen")
+    search_fields = ("nombre", "apellido")
+    list_filter = ("pais_origen",)
+    ordering = ("apellido", "nombre")
+    date_hierarchy = ("fec_nac")
